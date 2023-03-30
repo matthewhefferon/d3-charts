40d8642cfed606ab2d97548ba699216e86790e99
Matthew Hefferon <matthefferon@gmail.com>
2023-03-30 16:22:31 -0600

    Just show bar chart by default

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import "./App.css";
 import Bar from "./componets/Bar";
 import Line from "./componets/Line";
 import Pie from "./componets/Pie";
+import Scatter from "./componets/Scatter";
 
 function App() {
   return (
     <div className="App">
       <div>
         <Bar />
-      </div>
-      <div>
-        <Line />
-      </div>
-      <div>
+        {/* <Line />
         <Pie />
+        <Scatter /> */}
       </div>
     </div>
   );
